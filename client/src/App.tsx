ff4ac6b022491bc3dff953b52ee45e45a58a31f0
izzat <izzatfadzlon@gmail.com>
2025-06-14 08:37:05 +0800

    working daisyui + tailwind + ts starting point!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client";
-import client from "./lib/apollo";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import DishList from "./pages/DishList";
-import DishDetail from "./pages/DishDetail";
-import AddDish from "./pages/AddDish";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Profile from "./pages/Profile";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="min-h-screen bg-base-200">
-          <Navbar />
-          <main className="container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/dishes" element={<DishList />} />
-              <Route path="/dishes/:id" element={<DishDetail />} />
-              <Route path="/add-dish" element={<AddDish />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </ApolloProvider>
+    <>
+      <btn className="btn btn-primary">Click me</btn>
+    </>
   );
 }
 
