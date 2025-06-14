ac4df590fada08822d29e4c6b8d4644cb637ae07
izzat <izzatfadzlon@gmail.com>
2025-06-14 14:31:54 +0800

    feat: implement core features for RateMyDish app including routing, authentication, and dish management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import DishList from "./pages/DishList";
+import DishDetail from "./pages/DishDetail";
+import AddDish from "./pages/AddDish";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import client from "./lib/apollo";
+import { ApolloProvider } from "@apollo/client";
 
 function App() {
   return (
-    <>
-      <btn className="btn btn-primary">Click me</btn>
-    </>
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <div className="min-h-screen bg-base-200">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dishes" element={<DishList />} />
+              <Route path="/dishes/:id" element={<DishDetail />} />
+              <Route path="/add-dish" element={<AddDish />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </main>
+        </div>
+      </HashRouter>
+    </ApolloProvider>
   );
 }
 
